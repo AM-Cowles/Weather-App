ec1e0d21f49c5b7862a39c2ccc31bbb61150ec77
AM-Cowles <84042091+AM-Cowles@users.noreply.github.com>
2021-07-05 22:34:54 -0400

    added more script

--- script.js
+++ script.js
@@ -22,6 +22,19 @@ function displayCityWeather(city) {
         }).then(function(response) {
             console.log(response);
             var date = moment.unix(response.dt).format("MM/DD/YYYY");
+            $("#show_city").text(city + " (" + date + ")");
+
+            var temperature = ((response.main.temp - 273.15)*(9/5) + 32).toFixed(1);
+            $("#temp_val").text(temperature + " °F");
+            var wind_speed = response.wind.speed;
+            $("#wind_val").text(wind_speed + " MPH");
+
+            var iconcode = response.weather[0].icon;
+            var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            let locationIcon = $('.weather-icon');
+            $('locationIcon').attr('src', iconurl);
+
+            
         })
     }
 }
